ffdc65e8ab59aa594d10c147f52d2f4649cf5ac0
Rishabh Gupta <r.g.gupta@outlook.com>
2018-05-23 20:51:53 +0530

    Added color scheme

--- src/App.js
+++ src/App.js
@@ -7,15 +7,17 @@ import './App.css';
 class App extends Component {
   render() {
     let sectionStyle = { paddingTop: "10vh", paddingBottom: "10vh", height: "100vh", textAlign: "center" };
+    let styleIntro = { backgroundColor: "#6D9DA1" };
+    let styleSocl = { backgroundColor: "#F4E8AC" };
 
     return (
       <div className="App">
         <div className="container-fluid" >
           <div className="row">
-            <div className="col-sm" style={sectionStyle}>
+            <div className="col-sm" style={{...sectionStyle, ...styleIntro}}>
               <Introduce />
             </div>
-            <div className="col-sm" style={{ ...sectionStyle}}>
+            <div className="col-sm" style={{ ...sectionStyle, ...styleSocl}}>
               <SocialHandle />
             </div>
           </div>
--- src/Components/Introduce.jsx
+++ src/Components/Introduce.jsx
@@ -3,7 +3,7 @@ import myPic from '../resources/mypic.jpg';
 
 
 const Introduce = (props) => {
-  let imageStyle = { width: "40%", opacity: "0.7", marginBottom: "4vh" };
+  let imageStyle = { width: "40%", opacity: "0.9", marginBottom: "4vh" };
   return (
     <React.Fragment>
       <img
@@ -13,7 +13,7 @@ const Introduce = (props) => {
         style={imageStyle}
       />
       <h1>I'm Rishabh Gupta</h1>
-      <h1>and I create software</h1>
+      <h2>I create software</h2>
     </React.Fragment>
   )
 }
